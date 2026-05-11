5c4f068721dcd20df7153b17aa684f7397d58522
Research Dhamala <dhamala80130@gmail.com>
2026-05-11 18:40:28 +0545

    put the button on the place

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -15,11 +15,11 @@ const HeroSection = () => {
       />
 
       {/* Overlay */}
-      <div className="absolute right-[5px] bottom-[8%]  inset-0 flex flex-col items-end justify-end p-4  sm:px-32 md:p-10">
+      <div className="absolute  inset-0 flex flex-col items-end justify-end p-4  md:px-32 md:py-22 md:p-10">
         {/* ✅ Add new elements here — they'll stack above the button */}
         {/* <p className="text-white text-lg mb-4">Some new element</p> */}
 
-        <button className="bg-[#CF202E] text-white text-base md:text-xl font-bold py-3 md:py-4 px-10 sm:px-16 md:px-32 rounded-xl hover:bg-red-700 transition-colors uppercase tracking-wider">
+        <button className="bg-[#CF202E] text-white text-base md:text-xl font-bold py-3 md:py-4 px-10  md:px-32 rounded-xl hover:bg-white hover:text-[#CF202E] hover:border-2 hover:border-[#CF202E] sm:border-[#CF202E] sm:border-2 cursor-pointer  transition-all ">
           GET INVITED
         </button>
       </div>
